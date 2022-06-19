35a908cbe44e67accc340c6b7efe343ee1b05426
masya2021 <azarro@tut.by>
2022-06-19 23:41:02 +0300

    feat secondDays

---
+++ Scripts/scrollLoad.js
@@ -0,0 +1,24 @@
+import { createCardPhoto } from "./createCardPhoto.js";
+import { getData } from "./getData.js";
+
+export const scrollLoad = (gallery, grid, endElement) => {
+  const observer = new IntersectionObserver(
+    async (entries) => {
+      if (entries[0].isIntersecting) {
+        const photos = await getData();
+        const cards = photos.map(createCardPhoto);
+
+        Promise.all(cards).then((cards) => {
+          gallery.append(...cards);
+          grid.appended(cards);
+        });
+      }
+    },
+
+    {
+      rootMargin: "190px",
+    }
+  );
+
+  observer.observe(endElement);
+};
